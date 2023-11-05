64a75eb1c61f1080db294a72b9a1f21e1d584bf0
Bruno Galvao <bruno_galvao@outlook.com>
2023-11-05 19:21:46 -0300

    update: ant design add

--- src/app/components/Conteudo.tsx
+++ src/app/components/Conteudo.tsx
@@ -1,13 +1,28 @@
 'use client';
 
 import { FloatButton } from 'antd';
+import { Anchor } from 'antd';
 
 import Card from "./Card";
+import Footer from './Footer';
+import { ArrowUpOutlined } from '@ant-design/icons';
 
 export default function Content() {
 
-  const goToEnd = () => {
-    window.location.href = "#end";
+  const goToHome = () => {
+    window.location.href = "#Home";
+  }
+
+  const goToSkill = () => {
+    window.location.href = "#Skill";
+  }
+
+  const goToProject = () => {
+    window.location.href = "#Project";
+  }
+
+  const goToContact = () => {
+    window.location.href = "#Contact";
   };
 
   const gotToTop = () => {
@@ -16,39 +31,70 @@ export default function Content() {
 
   return (
     <div className="content">
-      <nav className="flex flex-row gap-3 list-none h-11 items-center mt-3">
-        <li onClick={goToEnd}>home</li>
-        <li>home</li>
-        <li>home</li>
-        <li>home</li>
-      </nav>
+      <div className="content-wrapper">
+        {/* <nav className="flex flex-row gap-3 list-none h-11 items-center mt-3">
+          <li onClick={goToHome}>home</li>
+          <li onClick={goToSkill}>skill</li>
+          <li onClick={goToProject}>project</li>
+          <li onClick={goToContact}>Contact</li>
+      </nav> */}
 
-      <Card
-        title="Titulo do Card"
-        content="aqui será o conteúdo"
-        link="https://picsum.photos/id/31/400/400" />
+        <Anchor
+          direction="horizontal"
+          className='h-20 bg-white flex items-center'
+          items={[
+            {
+              key: 'Home',
+              href: '#Home',
+              title: 'Home',
+            },
+            {
+              key: 'Skill',
+              href: '#Skill',
+              title: 'Skill',
+            },
+            {
+              key: 'Project',
+              href: '#Project',
+              title: 'Project',
+            },
+            {
+              key: 'Contact',
+              href: '#Contact',
+              title: 'Contact',
+            }
+          ]}
+        />
 
-      <section>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum omnis sint laborum, inventore consectetur perferendis nulla deserunt tempore voluptatum, magnam incidunt mollitia libero quod fugiat dolorum recusandae pariatur! Ab, voluptatem.
+
+        <section id='Home'>
+          <h2 className='font-bold'>Home</h2>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum omnis sint laborum, inventore consectetur perferendis nulla deserunt tempore voluptatum, magnam incidunt mollitia libero quod fugiat dolorum recusandae pariatur! Ab, voluptatem.
+        </section>
+        <section id='Skill'>
+          <h2 className='font-bold'>Skill</h2>
+          <Card
+            title="Titulo do Card"
+            content="aqui será o conteúdo"
+            link="https://picsum.photos/id/31/400/400" />
       </section>
-      <section>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum omnis sint laborum, inventore consectetur perferendis nulla deserunt tempore voluptatum, magnam incidunt mollitia libero quod fugiat dolorum recusandae pariatur! Ab, voluptatem.
+        <section id='Project'>
+          <h2 className='font-bold'>Project</h2>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum omnis sint laborum, inventore consectetur perferendis nulla deserunt tempore voluptatum, magnam incidunt mollitia libero quod fugiat dolorum recusandae pariatur! Ab, voluptatem.
       </section>
-      <section>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum omnis sint laborum, inventore consectetur perferendis nulla deserunt tempore voluptatum, magnam incidunt mollitia libero quod fugiat dolorum recusandae pariatur! Ab, voluptatem.
-      </section>
-      <section>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum omnis sint laborum, inventore consectetur perferendis nulla deserunt tempore voluptatum, magnam incidunt mollitia libero quod fugiat dolorum recusandae pariatur! Ab, voluptatem.
-      </section>
-      <section>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum omnis sint laborum, inventore consectetur perferendis nulla deserunt tempore voluptatum, magnam incidunt mollitia libero quod fugiat dolorum recusandae pariatur! Ab, voluptatem.
-      </section>
-      <section id="end">
+        <section id="Contact">
+          <h2 className='font-bold'>Contatos</h2>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum omnis sint laborum, inventore consectetur perferendis nulla deserunt tempore voluptatum, magnam incidunt mollitia libero quod fugiat dolorum recusandae pariatur! Ab, voluptatem.
       </section>
 
 
-      <FloatButton onClick={gotToTop} type="primary" style={{ right: 24 }} />
+        <FloatButton
+          onClick={gotToTop}
+          style={{ right: 20 }}
+          icon={<ArrowUpOutlined className='text-orange-500' />}
+          tooltip="Voltar ao inicio" />
+      </div>
+      <Footer />
     </div >
   )
 }
---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="content-wrapper">
+        <p>Desenvolvido por <a href="">Bruno Galvão</a></p>
+      </div>
+    </div>
+  )
+}

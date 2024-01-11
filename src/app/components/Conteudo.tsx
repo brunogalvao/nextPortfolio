8d6948289fd0b7e725bbdb265afcd4c9d3705fcb
Bruno Galvão <bruno_galvao@outlook.com>
2024-01-11 12:56:45 +0000

    fix: nome projeto

--- src/app/components/Conteudo.tsx
+++ src/app/components/Conteudo.tsx
@@ -50,27 +50,31 @@ export default function Content() {
             }
           ]}
         />
+
         </ConfigProvider>
 
         <section id='Home'>
           <h2 className='font-bold'>Home</h2>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum omnis sint laborum, inventore consectetur perferendis nulla deserunt tempore voluptatum, magnam incidunt mollitia libero quod fugiat dolorum recusandae pariatur! Ab, voluptatem.
         </section>
+        
         <section id='Skill'>
           <h2 className='font-bold'>Skill</h2>
           <Card
             title="Titulo do Card"
             content="aqui será o conteúdo"
             link="https://picsum.photos/id/31/400/400" />
-      </section>
+        </section>
+
         <section id='Project'>
-          <h2 className='font-bold'>Project</h2>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum omnis sint laborum, inventore consectetur perferendis nulla deserunt tempore voluptatum, magnam incidunt mollitia libero quod fugiat dolorum recusandae pariatur! Ab, voluptatem.
-      </section>
+            <h2 className='font-bold'>Project</h2>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum omnis sint laborum, inventore consectetur perferendis nulla deserunt tempore voluptatum, magnam incidunt mollitia libero quod fugiat dolorum recusandae pariatur! Ab, voluptatem.
+        </section>
+
         <section id="Contact">
-          <h2 className='font-bold'>Contatos</h2>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum omnis sint laborum, inventore consectetur perferendis nulla deserunt tempore voluptatum, magnam incidunt mollitia libero quod fugiat dolorum recusandae pariatur! Ab, voluptatem.
-      </section>
+            <h2 className='font-bold'>Contatos</h2>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum omnis sint laborum, inventore consectetur perferendis nulla deserunt tempore voluptatum, magnam incidunt mollitia libero quod fugiat dolorum recusandae pariatur! Ab, voluptatem.
+        </section>
 
 
         <FloatButton
@@ -79,7 +83,9 @@ export default function Content() {
           icon={<ArrowUpOutlined className='text-orange-500' />}
           tooltip="Voltar ao inicio" />
       </div>
+      
       <Footer />
+
     </div >
   )
 }
